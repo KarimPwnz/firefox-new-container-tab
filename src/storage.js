6ef2ae026455a8a3c9e7c19a71dc3749f549509b
Karim Rahal <karim@karimrahal.com>
2021-04-12 13:30:08 +0300

    Update storage.js

--- src/storage.js
+++ src/storage.js
@@ -2,8 +2,8 @@ export let storage = {
   get: async function (key) {
     return (await browser.storage.sync.get(key))[key];
   },
-  save: async function (key, value) {
-    browser.storage.sync.set({ [key]: value });
+  save: function (key, value) {
+    return browser.storage.sync.set({ [key]: value });
   },
 };
 
